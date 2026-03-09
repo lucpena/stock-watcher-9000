823646a073b12f226380811fdfbbf4273ee6aceb
Lucas Pena <lucaspena4@hotmail.com>
2026-03-09 17:02:05 -0300

    First Commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button";
+
+const Home: () => React.JSX.Element = () => {
+    return (
+        <div className="flex min-h-screen home-wrapper">
+            Home
+        </div>
+    )
+}
+
+export default Home
